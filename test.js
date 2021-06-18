9035ed462dc531fb5a2477b7dbc358e025b5b471
connor <connor@remi21.eu>
2021-06-18 14:11:42 +0100

    Adds test for random quote by author

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const { matchers } = require('jest-json-schema');
 expect.extend(matchers);
 
-const { getRandomBreakingBadQuote } = require('./index.js');
+const { getRandomBreakingBadQuote, getRandomBreakingBadQuoteByAuthor } = require('./index.js');
 
 it('should return a random quote', async () => {
   const quote = await getRandomBreakingBadQuote();
@@ -15,4 +15,20 @@ it('should return a random quote', async () => {
     required: ['quote_id', 'quote', 'author', 'series']
   };
   expect(quote).toMatchSchema(schema);
+});
+
+it('should return a random quote by author',  async () => {
+  const author = 'Jesse Pinkman';
+  const quote = await getRandomBreakingBadQuoteByAuthor(author);
+  const schema = {
+    properties: {
+      quote_id: { type: 'number' },
+      quote: { type: 'string' },
+      author: { type: 'string' },
+      series: { type: 'string' }
+    },
+    required: ['quote_id', 'quote', 'author', 'series']
+  };
+  expect(quote[0]).toMatchSchema(schema);
+  expect(quote[0].author).toBe('Jesse Pinkman');
 });
